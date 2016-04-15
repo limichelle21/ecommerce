e79abafb2352711593b187a5d58126f03362f2b2
Michelle Li <limichelle21@gmail.com>
2016-04-15 08:01:48 +0700

    created product model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160331134617) do
+ActiveRecord::Schema.define(version: 20160415010013) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "sku"
+    t.decimal  "price"
+    t.integer  "count"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
---
+++ spec/factories/products.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :product do
+    title "MyString"
+    description "MyText"
+    sku 1
+    price "9.99"
+    count 1
+  end
+end
