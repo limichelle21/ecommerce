c2e5c98f3e99ff55eb7d395f22b4278c0c6e17e9
Michelle Li <limichelle21@gmail.com>
2016-04-15 07:58:51 +0700

    user model with roles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160331050619) do
+ActiveRecord::Schema.define(version: 20160331134617) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -26,6 +26,12 @@ ActiveRecord::Schema.define(version: 20160331050619) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "type"
+    t.string   "name"
+    t.string   "username"
+    t.string   "address_1"
+    t.string   "address_2"
+    t.string   "phone_number"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
